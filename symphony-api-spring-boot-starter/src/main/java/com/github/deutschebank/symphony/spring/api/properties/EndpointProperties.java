c227529f4097a2d0dc729c2b34d7ad6bc009b1d7
Rob Moffat <robmoffat@mac.com>
2019-10-28 15:54:03 +0000

    Performed package rename

---
+++ symphony-api-spring-boot-starter/src/main/java/com/github/deutschebank/symphony/spring/api/properties/EndpointProperties.java
@@ -0,0 +1,60 @@
+package com.github.deutschebank.symphony.spring.api.properties;
+
+import javax.net.ssl.TrustManager;
+import javax.validation.constraints.NotNull;
+
+import com.symphony.api.bindings.ApiWrapper;
+import com.symphony.api.bindings.ConfigurableApiBuilder;
+import com.symphony.api.id.SymphonyIdentity;
+
+/**
+ * Encapsulates the details needed to connect to one of the Symphony endpoints.
+ * 
+ * @author Rob Moffat
+ *
+ */
+public class EndpointProperties {
+
+	@NotNull private String url;
+	
+	private ProxyProperties proxy;
+	
+
+	
+	public ProxyProperties getProxy() {
+		return proxy;
+	}
+	public void setProxy(ProxyProperties proxy) {
+		this.proxy = proxy;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+	public void configure(ConfigurableApiBuilder ab, ApiWrapper[] wrappers, SymphonyIdentity id, TrustManager[] trustManagers) {
+		ab.setUrl(getUrl());
+		ab.setKeyManagers(id.getKeyManagers());
+		ab.setTrustManagers(trustManagers);
+		ab.setWrappers(wrappers);
+		
+		if (proxy != null) {
+			proxy.configure(ab);
+		}
+	}
+	
+	public void configure(ConfigurableApiBuilder ab, ApiWrapper[] apiWrappers, TrustManager[] trustManagers) {
+		ab.setUrl(getUrl());
+		ab.setWrappers(apiWrappers);
+		ab.setTrustManagers(trustManagers);
+		
+		if (proxy != null) {
+			proxy.configure(ab);
+		}
+	}
+	
+}
+
---
+++ symphony-app-spring-boot-starter/src/main/java/com/github/deutschebank/symphony/spring/app/auth/PodAuthController.java
@@ -0,0 +1,101 @@
+package com.github.deutschebank.symphony.spring.app.auth;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+import com.github.deutschebank.symphony.spring.app.AbstractJsonController;
+import com.github.deutschebank.symphony.spring.app.SymphonyAppProperties;
+import com.github.deutschebank.symphony.spring.app.SymphonyAppProperties.CircleOfTrust;
+import com.github.deutschebank.symphony.spring.app.tokens.app.AppTokenStrategy;
+import com.github.deutschebank.symphony.spring.app.tokens.pod.PodTokenStrategy;
+import com.symphony.api.id.SymphonyIdentity;
+import com.symphony.api.model.ExtensionAppTokens;
+
+/**
+ * This handles the circle-of-trust concerns of the application, by
+ * connecting to a given symphony pod, and getting back an authentication token (Ta) and generating
+ * a token for the client (Ts).  
+ * 
+ * @see https://developers.symphony.com/extension/docs/application-authentication
+ * 
+ * @author Rob Moffat
+ *
+ */
+public class PodAuthController extends AbstractJsonController {
+	
+	public static final Logger LOG = LoggerFactory.getLogger(PodAuthController.class);
+	
+	public static final String POD_AUTH_PATH = "/podAuth";
+
+	private List<PodTokenStrategy> podTokenStrategies;
+	
+	private AppTokenStrategy appTokenStrategy;
+	
+	private CircleOfTrust circleOfTrust;
+	
+	public PodAuthController(SymphonyAppProperties p, View v, SymphonyIdentity appIdentity, AppTokenStrategy appTokenStrategy, List<PodTokenStrategy> tokenStrategies) {
+		super(p, v, appIdentity);
+		this.podTokenStrategies = tokenStrategies;
+		this.appTokenStrategy = appTokenStrategy;
+		this.circleOfTrust = p.getCircleOfTrust();
+	}
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String qs = request.getQueryString();
+		Map<String, String> params = decodeQuery(qs);
+		Map<String, Object> appData = new HashMap<>();
+		appData.put("circleOfTrust", circleOfTrust);
+		appData.put("appId", appIdentity.getCommonName());
+		
+		if (params.containsKey("podId")) {
+			
+			// do circle of trust if pod id is set.
+			String podId = params.get("podId");
+			String appToken = appTokenStrategy.generateAppToken();
+			ExtensionAppTokens tokens = queryStrategies(podId, appToken);
+			appTokenStrategy.storeAppToken(appToken, tokens.getSymphonyToken());
+			appData.put("tokenA", tokens.getAppToken());
+			appData.put("appId", tokens.getAppId());
+			appData.put("expireAt", tokens.getExpireAt());	
+		}
+		
+		ModelAndView mv = new ModelAndView(v, appData);
+		return mv;
+	}
+
+	public ExtensionAppTokens queryStrategies(String podId, String appToken) {
+		for (PodTokenStrategy podTokenStrategy : podTokenStrategies) {
+			ExtensionAppTokens tokens = null;
+			try {
+				tokens = podTokenStrategy.getTokens(appToken, podId);
+			} catch (Exception e) {
+				LOG.warn("Received error getting pod tokens", e);
+			}
+			
+			if (tokens != null) {
+				return tokens;
+			}
+		}
+		
+		throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Couldn't connect to back-end pod for token");
+	}
+
+
+	@Override
+	public String getPath() {
+		return p.getAppPath() + POD_AUTH_PATH;
+	}
+
+}
