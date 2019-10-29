7a4cf68cd31199ca46754911dfbd3e51396b273d
Rob Moffat <robmoffat@mac.com>
2019-10-29 11:15:52 +0000

    Improving readme

--- symphony-app-spring-boot-starter/src/main/java/com/github/deutschebank/symphony/spring/app/config/SymphonyAppWebSecurityConfigurer.java
+++ symphony-app-spring-boot-starter/src/main/java/com/github/deutschebank/symphony/spring/app/config/SymphonyAppWebSecurityConfigurer.java
@@ -1,6 +1,7 @@
 package com.github.deutschebank.symphony.spring.app.config;
 
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @PropertySource("classpath:/ssl/ssl-defaults.properties")
+@Order(99)
 public class SymphonyAppWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 }
