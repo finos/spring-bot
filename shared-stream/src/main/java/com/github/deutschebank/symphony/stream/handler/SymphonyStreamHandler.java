1445d71f2e83c497da1f9ba42483e87eb2b6d5a8
Rob Moffat <robmoffat@mac.com>
2020-05-15 11:56:46 +0100

    Improved error handling and testing with SharedStream

--- shared-stream/src/main/java/com/github/deutschebank/symphony/stream/handler/SymphonyStreamHandler.java
+++ shared-stream/src/main/java/com/github/deutschebank/symphony/stream/handler/SymphonyStreamHandler.java
@@ -2,9 +2,11 @@ package com.github.deutschebank.symphony.stream.handler;
 
 import java.util.function.Consumer;
 
+import javax.ws.rs.BadRequestException;
+
 import com.symphony.api.agent.DatafeedApi;
 import com.symphony.api.bindings.Streams;
-import com.symphony.api.model.Datafeed;
+import com.symphony.api.bindings.Streams.Worker;
 import com.symphony.api.model.V4Event;
 
 /**
@@ -14,12 +16,18 @@ import com.symphony.api.model.V4Event;
  *
  */
 public class SymphonyStreamHandler {
+	
+	public static final long MIN_BACK_OFF_MS = 2000;   // 2 secs
+	public static final long MAX_BACK_OFF_MS = 600000;	// 10 mins
+	
 
 	protected boolean running = false;
 	protected Consumer<V4Event> consumer;
 	protected DatafeedApi datafeedApi;
 	protected Consumer<Exception> exceptionHandler;
 	protected Thread runThread;
+	protected long currentBackOff = MIN_BACK_OFF_MS;
+	protected Worker<V4Event> worker;
 
 	public SymphonyStreamHandler(DatafeedApi api, Consumer<V4Event> eventConsumer, Consumer<Exception> exceptionHandler, boolean start) {
 		this.datafeedApi = api;
@@ -36,31 +44,46 @@ public class SymphonyStreamHandler {
 	public void start() {
 		if (running == false) {
 			running = true;
-			runThread = new Thread(() -> consumeLoop());
+			String initialDatafeedId = datafeedApi.v4DatafeedCreatePost(null, null).getId();
+			runThread = new Thread(() -> consumeLoop(initialDatafeedId));
 			runThread.setDaemon(true);
 			runThread.setName("SymphonyStream");
 			runThread.start();
 		}
 	}
 
-	public void consumeLoop() {
+	public void consumeLoop(String initialDatafeedId) {
 		while (running) {
+			String[] theId = { initialDatafeedId };
 			try {
-				Datafeed df = datafeedApi.v4DatafeedCreatePost(null, null);
-				Streams.createWorker(
-						() -> datafeedApi.v4DatafeedIdReadGet(df.getId(), null, null, 50),
-						exceptionHandler)
-					.stream().forEach(event -> sendToConsumer(event));
+				worker = Streams.createWorker(
+						() -> datafeedApi.v4DatafeedIdReadGet(theId[0], null, null, 50),
+						e -> {
+							exceptionHandler.accept(e);
+							if (e instanceof BadRequestException) {
+								theId[0] = datafeedApi.v4DatafeedCreatePost(null, null).getId();
+								backOff();
+							}
+						});
+				
+				worker.stream().forEach(event -> { 
+					sendToConsumer(event);
+					currentBackOff = MIN_BACK_OFF_MS;
+				});
 			} catch (Exception e) {
 				exceptionHandler.accept(e);
-				backOff();
 			}
 		}
 	}
 
+	/**
+	 * After a problem with the datafeed, we back-off, in order that we don't spam the exception handler with 
+	 * too many exception messages
+	 */
 	protected void backOff() {
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(currentBackOff);
+			currentBackOff = Math.min(MAX_BACK_OFF_MS, currentBackOff * 2);
 		} catch (InterruptedException e) {
 			exceptionHandler.accept(e);
 		}
@@ -72,6 +95,7 @@ public class SymphonyStreamHandler {
 	
 	public void stop() {
 		running = false;
+		worker.shutdown();
 		runThread.interrupt();
 	}
 }
---
+++ shared-stream/src/test/java/com/github/deutschebank/symphony/stream/handler/TestSymphonyStreamHandler.java
@@ -0,0 +1,249 @@
+package com.github.deutschebank.symphony.stream.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.BadRequestException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.symphony.api.agent.DatafeedApi;
+import com.symphony.api.model.Datafeed;
+import com.symphony.api.model.MessageList;
+import com.symphony.api.model.V2MessageList;
+import com.symphony.api.model.V4Event;
+import com.symphony.api.model.V4EventList;
+import com.symphony.api.model.V4Message;
+import com.symphony.api.model.V4MessageSent;
+import com.symphony.api.model.V4Payload;
+
+public class TestSymphonyStreamHandler {
+	
+	List<V4Event> events = new ArrayList<V4Event>();
+	List<Exception> exceptions = new ArrayList<Exception>();
+	
+	@Before
+	public void clear() {
+		events.clear();
+		exceptions.clear();
+	}
+	
+	class RuntimeInterruptedException extends RuntimeException {
+	}
+
+	
+	class DummyDatafeedApi implements DatafeedApi {
+		
+		int currentId = 868;
+		int call = 0;
+		int eventId = 0;
+
+		@Override
+		public Datafeed v1DatafeedCreatePost(String sessionToken, String keyManagerToken) {
+			return new Datafeed().id(""+(++currentId));
+		}
+
+		@Override
+		public MessageList v1DatafeedIdReadGet(String id, String sessionToken, String keyManagerToken,
+				Integer maxMessages) {
+			return null; // not used
+		}
+
+		@Override
+		public V2MessageList v2DatafeedIdReadGet(String id, String sessionToken, String keyManagerToken,
+				Integer maxMessages) {
+			return null; // not used
+		}
+
+		@Override
+		public Datafeed v4DatafeedCreatePost(String sessionToken, String keyManagerToken) {
+			return new Datafeed().id(""+(++currentId));
+		}
+
+		@Override
+		public V4EventList v4DatafeedIdReadGet(String id, String sessionToken, String keyManagerToken, Integer limit) {
+			if (id.equals(""+currentId)) {
+				return doInnerEventList();
+			} else {
+				throw new RuntimeException("Wrong id");
+			}
+		}
+
+		protected V4EventList doInnerEventList() {
+			if (call == 0) {
+				V4Event e = createEvent();
+				V4EventList out = new V4EventList();
+				out.add(e);
+				call++;
+				return out;
+			} else {
+				sleep(1000);
+				return null;
+			}
+		}
+
+		protected V4Event createEvent() {
+			return new V4Event().id(""+(eventId++)).payload(
+					new V4Payload().messageSent(
+							new V4MessageSent().message(
+								new V4Message().message("hi"))));
+		}
+	}
+
+	protected void sleep(long l) {
+		try {
+			Thread.sleep(l);
+		} catch (InterruptedException e) {
+			throw new RuntimeInterruptedException();
+		}
+	}
+
+
+	@Test
+	public void testGeneralOperation() throws InterruptedException {
+		SymphonyStreamHandler ssh = new SymphonyStreamHandler(new DummyDatafeedApi(), v -> events.add(v), e -> exceptions.add(e), true);
+		Thread.sleep(50);
+		Assert.assertEquals(1, events.size());
+		Assert.assertEquals(0, exceptions.size());
+		ssh.stop();
+	}
+	
+	@Test
+	public void testStopOperation() throws InterruptedException {
+		DummyDatafeedApi df = new DummyDatafeedApi() {
+			protected V4EventList doInnerEventList() {
+				sleep(150000);
+				throw new RuntimeException("Shouldn't get here");
+			}
+		};
+		
+		SymphonyStreamHandler ssh = new SymphonyStreamHandler(df, v -> events.add(v), e -> exceptions.add(e), false);
+		ssh.start();
+		Thread t = ssh.runThread;
+		Assert.assertEquals(true, t.isAlive());
+		Thread.sleep(50);
+		ssh.stop();
+		Thread.sleep(50);
+		Assert.assertEquals(0, events.size());
+		Assert.assertEquals(1, exceptions.size());
+		Assert.assertEquals(false, t.isAlive());
+		Assert.assertTrue(exceptions.get(0) instanceof RuntimeInterruptedException);
+	}
+	
+	@Test
+	public void testDodgyMessageProcessing() throws InterruptedException {
+		DummyDatafeedApi df = new DummyDatafeedApi() {
+			
+			/**
+			 * Just continuously spew messages
+			 */
+			public V4EventList v4DatafeedIdReadGet(String id, String sessionToken, String keyManagerToken, Integer limit) {
+				V4EventList out = new V4EventList();
+				out.add(createEvent());
+				out.add(createEvent());
+				out.add(createEvent());
+				out.add(createEvent());
+				out.add(createEvent());
+				return out;
+			}
+		};
+		
+		SymphonyStreamHandler ssh = new SymphonyStreamHandler(df, v -> {
+			if (v.getId().contains("5")) {
+				throw new IllegalArgumentException("Couldn't process: "+v.getId());
+			} else {
+				events.add(v);
+			}
+		}, e -> exceptions.add(e), true);
+		Thread.sleep(200);
+		
+		ssh.stop();
+		
+		// we should still be on the first datafeed
+		Assert.assertEquals(869, df.currentId);
+		
+		// assert no back-off for user errors
+		Assert.assertTrue(events.size() > 20);
+		
+		// assert that there are errors in the log
+		Assert.assertFalse(exceptions.isEmpty());
+	}
+	
+	@Test
+	public void testStreamDying() throws InterruptedException {
+		DummyDatafeedApi df = new DummyDatafeedApi() {
+			
+			int readFrom = 0;
+			/**
+			 * You can read from each datafeed only once, before it dies
+			 */
+			public V4EventList v4DatafeedIdReadGet(String id, String sessionToken, String keyManagerToken, Integer limit) {
+				if (readFrom == currentId) {
+					throw new BadRequestException("broken");
+				} else {
+					readFrom = currentId;
+					V4EventList out = new V4EventList();
+					out.add(createEvent());
+					out.add(createEvent());
+					out.add(createEvent());
+					out.add(createEvent());
+					out.add(createEvent());
+					return out;
+				}
+			}
+		};
+		
+		SymphonyStreamHandler ssh = new SymphonyStreamHandler(df, v -> events.add(v), e -> exceptions.add(e), true);
+		Thread.sleep(4000);
+		ssh.stop();
+		
+		// we should be after the first datafeed
+		Assert.assertTrue(df.currentId > 869);
+		
+		// check we've read lots of events
+		Assert.assertTrue(events.size() > 5);
+		
+		// should be errors from the datafeed dying
+		Assert.assertFalse(exceptions.isEmpty());
+		Assert.assertTrue(exceptions.get(0) instanceof BadRequestException);
+	}
+	
+	@Test(expected = BadRequestException.class)
+	public void testSymphonyDeadOnStartup() {
+		DummyDatafeedApi df = new DummyDatafeedApi() {
+
+			/**
+			 * Stream handler should throw exception on startup
+			 */
+			@Override
+			public Datafeed v4DatafeedCreatePost(String sessionToken, String keyManagerToken) {
+				throw new BadRequestException();
+			}
+		};
+		
+		SymphonyStreamHandler ssh = new SymphonyStreamHandler(df, v -> events.add(v), e -> exceptions.add(e), true);
+		ssh.stop();
+	}
+	
+	@Test
+	public void testBackoffExceptionSpam() throws InterruptedException {
+		DummyDatafeedApi df = new DummyDatafeedApi() {
+			
+			/**
+			 * Continually throw exceptions
+			 */
+			public V4EventList v4DatafeedIdReadGet(String id, String sessionToken, String keyManagerToken, Integer limit) {
+				throw new BadRequestException("broken");
+			}
+		};
+		
+		SymphonyStreamHandler ssh = new SymphonyStreamHandler(df, v -> events.add(v), e -> exceptions.add(e), true);
+		Thread.sleep(10000);
+		ssh.stop();
+		// initial back-off is 2 secs, but we should only see 3 exceptions in the log.
+		Assert.assertTrue(exceptions.size() == 3);
+		
+	}
+}
