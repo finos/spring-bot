902b212275fa88afa1c49bff9a2cec15daee1dcb
Robert Moffat <robert.moffat@db.com>
2020-12-01 13:06:31 +0000

    Compiling, not really working properly

---
+++ koreai/src/main/java/org/finos/symphony/toolkit/koreai/spring/KoreAIBridgeFactory.java
@@ -0,0 +1,13 @@
+package org.finos.symphony.toolkit.koreai.spring;
+
+import org.finos.symphony.toolkit.stream.handler.SymphonyStreamHandler;
+
+public interface KoreAIBridgeFactory {
+
+	/**
+	 * Constructs the bridge for a given {@link KoreAIInstanceProperties}, which defines the
+	 * mapping between symphony bot and koreAI bot.
+	 */
+	SymphonyStreamHandler buildBridge(KoreAIInstanceProperties props);
+	
+}
--- koreai/src/main/java/org/finos/symphony/toolkit/koreai/spring/KoreAIConfig.java
+++ koreai/src/main/java/org/finos/symphony/toolkit/koreai/spring/KoreAIConfig.java
@@ -1,50 +1,81 @@
-package org.finos.symphony.toolkit.koreai;
+package org.finos.symphony.toolkit.koreai.spring;
 
-import java.io.IOException;
+import javax.inject.Named;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.finos.symphony.toolkit.json.EntityJsonTypeResolverBuilder.VersionSpace;
 import org.finos.symphony.toolkit.json.ObjectMapperFactory;
-import org.finos.symphony.toolkit.koreai.output.KoreAIResponseHandler;
-import org.finos.symphony.toolkit.koreai.output.KoreAIResponseHandlerImpl;
-import org.finos.symphony.toolkit.koreai.request.KoreAIRequester;
-import org.finos.symphony.toolkit.koreai.request.KoreAIRequesterImpl;
 import org.finos.symphony.toolkit.koreai.response.KoreAIResponse;
-import org.finos.symphony.toolkit.koreai.response.KoreAIResponseBuilder;
-import org.finos.symphony.toolkit.koreai.response.KoreAIResponseBuilderImpl;
+import org.finos.symphony.toolkit.spring.api.ApiInstanceFactory;
+import org.finos.symphony.toolkit.spring.api.SymphonyApiConfig;
+import org.finos.symphony.toolkit.spring.api.properties.PodProperties;
+import org.finos.symphony.toolkit.spring.api.properties.SymphonyApiProperties;
+import org.finos.symphony.toolkit.stream.Participant;
+import org.finos.symphony.toolkit.stream.cluster.transport.Multicaster;
+import org.finos.symphony.toolkit.stream.handler.SymphonyStreamHandler;
+import org.finos.symphony.toolkit.stream.log.LogMessage;
+import org.finos.symphony.toolkit.stream.spring.SharedStreamConfig.ExceptionConsumer;
+import org.finos.symphony.toolkit.stream.spring.SharedStreamWebConfig;
+import org.finos.symphony.toolkit.stream.spring.SymphonyStreamProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.ResourceLoader;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.symphony.api.agent.MessagesApi;
-import com.symphony.api.id.SymphonyIdentity;
-import com.symphony.api.model.User;
-import com.symphony.api.pod.UsersApi;
 
 @Configuration
-@EnableConfigurationProperties(KoreaiProperties.class)
-public class KoreAIConfig {
+@AutoConfigureAfter({SymphonyApiConfig.class, SharedStreamWebConfig.class})
+@EnableConfigurationProperties({KoreAIProperties.class, SymphonyStreamProperties.class, SymphonyApiProperties.class})
+public class KoreAIConfig implements InitializingBean {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(KoreAIConfig.class);
 
-	@Autowired
-	KoreaiProperties properties;
+	public static final String KORE_AI_BRIDGE_FACTORY_BEAN = "koreAiBridgeFactory";
 	
 	@Autowired
-	SymphonyIdentity botIdentity;
+	KoreAIProperties koreProperties;
 	
 	@Autowired
-	MessagesApi messagesApi;
+	SymphonyStreamProperties streamProperties;
+	
+	@Autowired
+	SymphonyApiProperties apiProperties;
 	
 	@Autowired
 	ResourceLoader rl;
 	
-	@Autowired
-	UsersApi usersApi;
+	@Autowired 
+	ApiInstanceFactory symphonyAPIInstanceFactory;
 	
+	@Autowired
+	ApplicationContext ctx;
+	
+	@Autowired
+	ExceptionConsumer ec;
+
+	@Autowired
+	Multicaster mc;
+
+	@Autowired
+	Participant participant;
+
+	@Autowired(required = false)
+	@Named(SymphonyApiConfig.SYMPHONY_TRUST_MANAGERS_BEAN)
+	TrustManagerFactory tmf;
+		
 	@Bean
+	@ConditionalOnMissingBean
 	public ObjectMapper symphonyObjectMapper() {
 		return koreAIObjectMapper();
 	}
@@ -53,35 +84,52 @@ public class KoreAIConfig {
 		ObjectMapper out = new ObjectMapper();
 		ObjectMapperFactory.initialize(out, ObjectMapperFactory
 			.extendedSymphonyVersionSpace(
+				LogMessage.VERSION_SPACE,	
 				new VersionSpace(KoreAIResponse.class.getPackage().getName(), "1.0"),
 				new VersionSpace(ObjectNode.class.getPackage().getName(), "1.0")));
 		return out;
 	}
 		
-	@Bean
-	public KoreAIResponseHandler responseMessageAdapter() throws IOException {
-		return new KoreAIResponseHandlerImpl(messagesApi, rl, 
-				properties.isSkipEmptyResponses(), 
-				symphonyObjectMapper(),
-				properties.getTemplatePrefix());	
-	}
 	
 	@Bean
-	public KoreAIRequester koreAIRequester(KoreAIResponseHandler responseHandler, KoreAIResponseBuilder responseBuilder) throws Exception {
-		return new KoreAIRequesterImpl(responseHandler,
-				responseBuilder,
-				properties.getUrl(), 
-				JsonNodeFactory.instance, 
-				properties.getJwt());
+	@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+	public SymphonyStreamHandler createHandler(KoreAIInstanceProperties props) {
+		return koreAIBridgeFactory().buildBridge(props);	
 	}
 	
-	@Bean
-	public KoreAIResponseBuilder koreAIResponseBuilder() {
-		return new KoreAIResponseBuilderImpl(new ObjectMapper(), JsonNodeFactory.instance);
+	
+	@Bean(name = KORE_AI_BRIDGE_FACTORY_BEAN)
+	@ConditionalOnMissingBean
+	public KoreAIBridgeFactory koreAIBridgeFactory() {
+		PodProperties pp;
+		if (apiProperties.getApis().size() != 1) {
+			throw new IllegalArgumentException("KoreAI Bridge must have the details of a single pod configured");
+		}
+		
+		pp = apiProperties.getApis().get(0);
+		
+		return new KoreAIBridgeFactoryImpl(
+			rl, 
+			koreAIObjectMapper(), 
+			tmf,
+			pp, 
+			koreProperties, 
+			streamProperties, 
+			symphonyAPIInstanceFactory, 
+			ec, 
+			mc, 
+			participant);
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (koreProperties.getInstances() == null) {
+			LOG.error("No KoreAI Bridge instances defined in symphony.koreai.instances");
+		} else {
+			for (KoreAIInstanceProperties instance : koreProperties.getInstances()) {
+				ctx.getBean(SymphonyStreamHandler.class, instance);
+			}
+		}
 	}
 	
-	@Bean
-	public KoreAIEventHandler koreAIEventHandler(KoreAIRequester requester) {
-		return new KoreAIEventHandler(botIdentity, usersApi, requester, symphonyObjectMapper(), properties.isOnlyAddressed());
-	}
 }
