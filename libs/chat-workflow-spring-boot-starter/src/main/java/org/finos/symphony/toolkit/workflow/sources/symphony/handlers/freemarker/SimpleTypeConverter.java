46c75a69b3fb5905119e44a2e78947050b146ccd
Rob Moffat <rob@kite9.com>
2021-04-22 11:54:14 +0100

    Working with Amsidh

---
+++ libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/sources/symphony/handlers/freemarker/SimpleTypeConverter.java
@@ -0,0 +1,8 @@
+package org.finos.symphony.toolkit.workflow.sources.symphony.handlers.freemarker;
+
+import org.finos.symphony.toolkit.json.EntityJson;
+
+public interface SimpleTypeConverter extends TypeConverter {
+
+	
+}
