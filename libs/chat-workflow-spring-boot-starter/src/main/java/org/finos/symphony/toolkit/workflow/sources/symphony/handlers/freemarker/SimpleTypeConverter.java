1d5e030ab6ddc05bdaacbf179f532ee01c7d107b
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-04-23 09:44:09 +0530

    Testing under progress for Collection Template

--- libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/sources/symphony/handlers/freemarker/SimpleTypeConverter.java
+++ libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/sources/symphony/handlers/freemarker/SimpleTypeConverter.java
@@ -1,7 +1,5 @@
 package org.finos.symphony.toolkit.workflow.sources.symphony.handlers.freemarker;
 
-import org.finos.symphony.toolkit.json.EntityJson;
-
 public interface SimpleTypeConverter extends TypeConverter {
 
 	
