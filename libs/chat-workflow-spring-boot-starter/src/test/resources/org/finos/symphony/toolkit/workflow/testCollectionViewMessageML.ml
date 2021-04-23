5af1ce1de19d746b52ad36d512ac45ff557c0078
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-04-23 09:44:09 +0530

    Testing under progress for Collection Template

--- libs/chat-workflow-spring-boot-starter/src/test/resources/org/finos/symphony/toolkit/workflow/testCollectionViewMessageML.ml
+++ libs/chat-workflow-spring-boot-starter/src/test/resources/org/finos/symphony/toolkit/workflow/testCollectionViewMessageML.ml
@@ -1,10 +1,9 @@
-
 <#-- starting template -->
  <table>
  <tr><td><b>Names:</b></td><td>
   <span class="tempo-text-color--red">${entity.errors.contents['names']!''}</span>
-  
-  <table><thead><tr><td><b>Value</b></td>
+
+  <table><thead><tr><td>Value</td>
   </tr></thead><tbody>
   <#list entity.workflow_001.names as iB>
   <tr><td>${iB!''}</td>
@@ -13,23 +12,23 @@
   </tbody></table></td></tr>
  <tr><td><b>Addresses:</b></td><td>
   <span class="tempo-text-color--red">${entity.errors.contents['addresses']!''}</span>
-  
+
   <table><thead><tr>
-  
-    <td><b>City</b></td>
+
+    <td ><b>City</b></td>
   </tr></thead><tbody>
   <#list entity.workflow_001.addresses as iB>
   <tr>
-  
+
    <td >${iB.city!''}</td>
   </tr>
   </#list>
   </tbody></table></td></tr>
  </table>
-<form 
+<form
   id="just-buttons-form">
   <p><#list entity.buttons.contents as button>
-    <button 
+    <button
          name="${button.name}"
          type="${button.buttonType?lower_case}">
       ${button.text}
