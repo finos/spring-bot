ff00b0ff5dee1d7edc68b5d6219208fba335ff2a
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-04-23 09:44:09 +0530

    Testing under progress for Collection Template

--- libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/sources/symphony/handlers/freemarker/ComplexTypeConverter.java
+++ libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/sources/symphony/handlers/freemarker/ComplexTypeConverter.java
@@ -1,13 +1,13 @@
 package org.finos.symphony.toolkit.workflow.sources.symphony.handlers.freemarker;
 
+import org.finos.symphony.toolkit.json.EntityJson;
+
 import java.lang.reflect.Field;
 import java.util.List;
 
-import org.finos.symphony.toolkit.json.EntityJson;
-
 public interface ComplexTypeConverter extends TypeConverter {
 
-	public String withFields(WithType controller, Class<?> c, boolean editMode, Variable variable, EntityJson ej, WithField displayer);
+	String withFields(WithType controller, Class<?> c, boolean editMode, Variable variable, EntityJson ej, WithField displayer);
 	
 	List<Field> getFields(Class<?> c);
 }
