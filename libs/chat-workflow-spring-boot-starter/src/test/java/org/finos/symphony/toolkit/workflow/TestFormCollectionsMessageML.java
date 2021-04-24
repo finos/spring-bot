f66b52d43b6963a104ead25bb0941f100edfffb6
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-04-24 15:34:15 +0530

    #171 Removed data file and added hardcoded data method due to text format issue

--- libs/chat-workflow-spring-boot-starter/src/test/java/org/finos/symphony/toolkit/workflow/TestFormCollectionsMessageML.java
+++ libs/chat-workflow-spring-boot-starter/src/test/java/org/finos/symphony/toolkit/workflow/TestFormCollectionsMessageML.java
@@ -72,7 +72,7 @@ public class TestFormCollectionsMessageML extends AbstractMockSymphonyTest {
 				ErrorHelp.createErrorHolder(), empty);
 		String json = ejc.writeValue(empty);
 		System.out.println("<messageML>" + out + "</messageML>\n"+json);
-		//Assertions.assertEquals(loadML("testCollectionsEditMessageML.ml"), out);
+		Assertions.assertEquals(getTestCollectionsEditMessage(), out);
 		compareJson(loadJson("testCollectionsEditMessageML.json"), json); 
 	}
 
@@ -89,7 +89,7 @@ public class TestFormCollectionsMessageML extends AbstractMockSymphonyTest {
 				ErrorHelp.createErrorHolder(), empty);
 		String json = ejc.writeValue(empty);
 		System.out.println("<messageML>" + out + "</messageML>\n"+json);
-		//Assertions.assertEquals(loadML("testCollectionsViewMessageML.ml"), out);
+		Assertions.assertEquals(getTestCollectionsViewMessage(), out);
 		compareJson(loadJson("testCollectionsViewMessageML.json"), json); 
 	}
 	
@@ -99,6 +99,161 @@ public class TestFormCollectionsMessageML extends AbstractMockSymphonyTest {
 	}
 
 
+ private String getTestCollectionsEditMessage(){
+		return "\n<#-- starting template -->\n" +
+				"<form \n" +
+				"  id=\"org.finos.symphony.toolkit.workflow.fixture.TestCollections\">\n" +
+				" <table>\n" +
+				" <tr><td><b>String List:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['stringList']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr><td><b>Value</b></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"stringList.table-delete-rows\">Delete</button></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"stringList.table-add-row\">New</button></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.formdata.stringList as iB>\n" +
+				"  <tr><td>${iB!''}</td>\n" +
+				"   <td style=\"text-align:center; width:10%\" ><checkbox name=\"stringList.${iB?index}.selected\" /></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"stringList[${iB?index}].table-edit-row\">Edit</button></td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" <tr><td><b>Enum List:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['enumList']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr><td><b>Value</b></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"enumList.table-delete-rows\">Delete</button></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"enumList.table-add-row\">New</button></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.formdata.enumList as iB>\n" +
+				"  <tr><td>${iB!''}</td>\n" +
+				"   <td style=\"text-align:center; width:10%\" ><checkbox name=\"enumList.${iB?index}.selected\" /></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"enumList[${iB?index}].table-edit-row\">Edit</button></td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" <tr><td><b>Min Bean List:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['minBeanList']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr>\n" +
+				"  \n" +
+				"    <td style=\"text-align:center; width:10%\" ><b>Some String</b></td>\n" +
+				"  \n" +
+				"    <td style=\"text-align: right;\"><b>Some Integer</b></td>\n" +
+				"  \n" +
+				"    <td style=\"text-align:center; width:10%\" ><b>Some More Strings</b></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"minBeanList.table-delete-rows\">Delete</button></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"minBeanList.table-add-row\">New</button></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.formdata.minBeanList as iB>\n" +
+				"  <tr>\n" +
+				"  \n" +
+				"   <td >${iB.someString!''}</td>\n" +
+				"  \n" +
+				"   <td style=\"text-align: right;\">${iB.someInteger!''}</td>\n" +
+				"  \n" +
+				"   <td >...</td>\n" +
+				"   <td style=\"text-align:center; width:10%\" ><checkbox name=\"minBeanList.${iB?index}.selected\" /></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"minBeanList[${iB?index}].table-edit-row\">Edit</button></td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" <tr><td><b>Some Hash Tags:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['someHashTags']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr><td><b>Value</b></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"someHashTags.table-delete-rows\">Delete</button></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"someHashTags.table-add-row\">New</button></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.formdata.someHashTags as iB>\n" +
+				"  <tr><td>\n" +
+				"  <#if iB??><cash \n" +
+				"   tag=\"${iB.name!''}\" /></#if></td>\n" +
+				"   <td style=\"text-align:center; width:10%\" ><checkbox name=\"someHashTags.${iB?index}.selected\" /></td>\n" +
+				"   <td style=\"text-align:center;\" ><button name=\"someHashTags[${iB?index}].table-edit-row\">Edit</button></td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" </table>\n" +
+				"  <p><#list entity.buttons.contents as button>\n" +
+				"    <button \n" +
+				"         name=\"${button.name}\"\n" +
+				"         type=\"${button.buttonType?lower_case}\">\n" +
+				"      ${button.text}\n" +
+				"    </button>\n" +
+				"  </#list></p>\n" +
+				"</form>\n" +
+				"<#-- ending template -->\n";
+ }
 
-
+ private String getTestCollectionsViewMessage(){
+		return "\n<#-- starting template -->\n" +
+				" <table>\n" +
+				" <tr><td><b>String List:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['stringList']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr><td><b>Value</b></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.workflow_001.stringList as iB>\n" +
+				"  <tr><td>${iB!''}</td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" <tr><td><b>Enum List:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['enumList']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr><td><b>Value</b></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.workflow_001.enumList as iB>\n" +
+				"  <tr><td>${iB!''}</td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" <tr><td><b>Min Bean List:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['minBeanList']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr>\n" +
+				"  \n" +
+				"    <td style=\"text-align:center; width:10%\" ><b>Some String</b></td>\n" +
+				"  \n" +
+				"    <td style=\"text-align: right;\"><b>Some Integer</b></td>\n" +
+				"  \n" +
+				"    <td style=\"text-align:center; width:10%\" ><b>Some More Strings</b></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.workflow_001.minBeanList as iB>\n" +
+				"  <tr>\n" +
+				"  \n" +
+				"   <td >${iB.someString!''}</td>\n" +
+				"  \n" +
+				"   <td style=\"text-align: right;\">${iB.someInteger!''}</td>\n" +
+				"  \n" +
+				"   <td >...</td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" <tr><td><b>Some Hash Tags:</b></td><td>\n" +
+				"  <span class=\"tempo-text-color--red\">${entity.errors.contents['someHashTags']!''}</span>\n" +
+				"  \n" +
+				"  <table><thead><tr><td><b>Value</b></td>\n" +
+				"  </tr></thead><tbody>\n" +
+				"  <#list entity.workflow_001.someHashTags as iB>\n" +
+				"  <tr><td>\n" +
+				"  <#if iB??><cash \n" +
+				"   tag=\"${iB.name!''}\" /></#if></td>\n" +
+				"  </tr>\n" +
+				"  </#list>\n" +
+				"  </tbody></table></td></tr>\n" +
+				" </table>\n" +
+				"<form \n" +
+				"  id=\"just-buttons-form\">\n" +
+				"  <p><#list entity.buttons.contents as button>\n" +
+				"    <button \n" +
+				"         name=\"${button.name}\"\n" +
+				"         type=\"${button.buttonType?lower_case}\">\n" +
+				"      ${button.text}\n" +
+				"    </button>\n" +
+				"  </#list></p>\n" +
+				"</form>\n" +
+				"<#-- ending template -->\n";
+ }
 }
