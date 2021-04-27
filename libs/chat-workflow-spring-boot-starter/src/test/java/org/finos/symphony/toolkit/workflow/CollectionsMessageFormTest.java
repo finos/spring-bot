43f27981edbc19c0e77cfb0d5ccde6a03aafce1b
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-04-27 18:31:49 +0530

    #143 Fixed issue of data formatting

--- libs/chat-workflow-spring-boot-starter/src/test/java/org/finos/symphony/toolkit/workflow/CollectionsMessageFormTest.java
+++ libs/chat-workflow-spring-boot-starter/src/test/java/org/finos/symphony/toolkit/workflow/CollectionsMessageFormTest.java
@@ -54,7 +54,7 @@ public class CollectionsMessageFormTest extends AbstractMockSymphonyTest {
                 ErrorHelp.createErrorHolder(), empty);
         String json = ejc.writeValue(empty);
         System.out.println("<messageML>" + out + "</messageML>\n" + json);
-        Assertions.assertEquals(getTestCollectionEditMessageML(), out);
+        Assertions.assertTrue(loadML("testCollectionEditMessageML.ml").contentEquals(out));
         compareJson(loadJson("testCollectionEditMessage.json"), json);
     }
 
@@ -71,7 +71,7 @@ public class CollectionsMessageFormTest extends AbstractMockSymphonyTest {
                 ErrorHelp.createErrorHolder(), empty);
         String json = ejc.writeValue(empty);
         System.out.println("<messageML>" + out + "</messageML>\n" + json);
-        Assertions.assertEquals(getTestCollectionViewMessageML(), out);
+        Assertions.assertTrue(loadML("testCollectionViewMessageML.ml").contentEquals(out));
         compareJson(loadJson("testCollectionViewMessage.json"), json);
     }
     private void compareJson(String loadJson, String json) throws JsonMappingException, JsonProcessingException {
@@ -83,101 +83,7 @@ public class CollectionsMessageFormTest extends AbstractMockSymphonyTest {
         Person person = new Person(Arrays.asList("abc","pqr"), Arrays.asList(new Address("Pune"), new Address("Mumbai"), new Address("Bangalore")));
         return person;
     }
-
-    private String getTestCollectionEditMessageML(){
-        return "\n" +
-                "<#-- starting template -->\n" +
-                "<form \n" +
-                "  id=\"org.finos.symphony.toolkit.workflow.Person\">\n" +
-                " <table>\n" +
-                " <tr><td><b>Names:</b></td><td>\n" +
-                "  <span class=\"tempo-text-color--red\">${entity.errors.contents['names']!''}</span>\n" +
-                "  \n" +
-                "  <table><thead><tr><td><b>Value</b></td>\n" +
-                "   <td style=\"text-align:center;\" ><button name=\"names.table-delete-rows\">Delete</button></td>\n" +
-                "   <td style=\"text-align:center;\" ><button name=\"names.table-add-row\">New</button></td>\n" +
-                "  </tr></thead><tbody>\n" +
-                "  <#list entity.formdata.names as iB>\n" +
-                "  <tr><td>${iB!''}</td>\n" +
-                "   <td style=\"text-align:center; width:10%\" ><checkbox name=\"names.${iB?index}.selected\" /></td>\n" +
-                "   <td style=\"text-align:center;\" ><button name=\"names[${iB?index}].table-edit-row\">Edit</button></td>\n" +
-                "  </tr>\n" +
-                "  </#list>\n" +
-                "  </tbody></table></td></tr>\n" +
-                " <tr><td><b>Addresses:</b></td><td>\n" +
-                "  <span class=\"tempo-text-color--red\">${entity.errors.contents['addresses']!''}</span>\n" +
-                "  \n" +
-                "  <table><thead><tr>\n" +
-                "  \n" +
-                "    <td style=\"text-align:center; width:10%\" ><b>City</b></td>\n" +
-                "   <td style=\"text-align:center;\" ><button name=\"addresses.table-delete-rows\">Delete</button></td>\n" +
-                "   <td style=\"text-align:center;\" ><button name=\"addresses.table-add-row\">New</button></td>\n" +
-                "  </tr></thead><tbody>\n" +
-                "  <#list entity.formdata.addresses as iB>\n" +
-                "  <tr>\n" +
-                "  \n" +
-                "   <td >${iB.city!''}</td>\n" +
-                "   <td style=\"text-align:center; width:10%\" ><checkbox name=\"addresses.${iB?index}.selected\" /></td>\n" +
-                "   <td style=\"text-align:center;\" ><button name=\"addresses[${iB?index}].table-edit-row\">Edit</button></td>\n" +
-                "  </tr>\n" +
-                "  </#list>\n" +
-                "  </tbody></table></td></tr>\n" +
-                " </table>\n" +
-                "  <p><#list entity.buttons.contents as button>\n" +
-                "    <button \n" +
-                "         name=\"${button.name}\"\n" +
-                "         type=\"${button.buttonType?lower_case}\">\n" +
-                "      ${button.text}\n" +
-                "    </button>\n" +
-                "  </#list></p>\n" +
-                "</form>\n" +
-                "<#-- ending template -->\n";
-    }
-
-
-    private String getTestCollectionViewMessageML() {
-        return "\n" +
-                "<#-- starting template -->\n" +
-                " <table>\n" +
-                " <tr><td><b>Names:</b></td><td>\n" +
-                "  <span class=\"tempo-text-color--red\">${entity.errors.contents['names']!''}</span>\n" +
-                "  \n" +
-                "  <table><thead><tr><td><b>Value</b></td>\n" +
-                "  </tr></thead><tbody>\n" +
-                "  <#list entity.workflow_001.names as iB>\n" +
-                "  <tr><td>${iB!''}</td>\n" +
-                "  </tr>\n" +
-                "  </#list>\n" +
-                "  </tbody></table></td></tr>\n" +
-                " <tr><td><b>Addresses:</b></td><td>\n" +
-                "  <span class=\"tempo-text-color--red\">${entity.errors.contents['addresses']!''}</span>\n" +
-                "  \n" +
-                "  <table><thead><tr>\n" +
-                "  \n" +
-                "    <td style=\"text-align:center; width:10%\" ><b>City</b></td>\n" +
-                "  </tr></thead><tbody>\n" +
-                "  <#list entity.workflow_001.addresses as iB>\n" +
-                "  <tr>\n" +
-                "  \n" +
-                "   <td >${iB.city!''}</td>\n" +
-                "  </tr>\n" +
-                "  </#list>\n" +
-                "  </tbody></table></td></tr>\n" +
-                " </table>\n" +
-                "<form \n" +
-                "  id=\"just-buttons-form\">\n" +
-                "  <p><#list entity.buttons.contents as button>\n" +
-                "    <button \n" +
-                "         name=\"${button.name}\"\n" +
-                "         type=\"${button.buttonType?lower_case}\">\n" +
-                "      ${button.text}\n" +
-                "    </button>\n" +
-                "  </#list></p>\n" +
-                "</form>\n" +
-                "<#-- ending template -->\n";
-    }
-
-
+    
 }
 
 class Person {
