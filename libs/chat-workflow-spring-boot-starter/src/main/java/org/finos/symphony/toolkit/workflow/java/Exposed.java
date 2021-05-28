69e0ed58828f5dbb863f5d11ae4f5453ecef40f5
Rob Moffat <rob@kite9.com>
2021-05-28 12:33:50 +0100

    HandlerMapping

--- libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/java/Exposed.java
+++ libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/java/Exposed.java
@@ -5,17 +5,41 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Exposed {
+	
+	public class NoFormClass {}
 
+	/**
+	 * A set of patterns that are matched against the user input.
+	 * Use '*' to match everything.
+	 */
 	String[] value();
 		
+	/**
+	 * Used for the help text.  
+	 */
 	String description() default "";
 	
+	/**
+	 * A list of rooms that the command works in.
+	 */
 	String[] rooms() default {};
 	
 	/**
+	 * Means that the command can only be done by room administrators.
+	 */
+	boolean admin() default false;
+	
+	/**
 	 * This means that the exposed method only applies buttons on a given form.
+	 * TODO: do we need this?
 	 */
 	String formName() default ""; 
+	
+	/**
+	 * Says that this method must be represented as a button when we display objects of 
+	 * this class.
+	 **/
+	Class<?> formClass() default NoFormClass.class;
 	
 	boolean addToHelp() default true;
 	
---
+++ libs/chat-workflow-spring-boot-starter/src/main/java/org/finos/symphony/toolkit/workflow/java/mapping/AbstractSpringComponentHandlerMapping.java
@@ -0,0 +1,399 @@
+package org.finos.symphony.toolkit.workflow.java.mapping;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.http.HttpServletRequest;
+
+import org.finos.symphony.toolkit.workflow.Action;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.support.ApplicationObjectSupport;
+import org.springframework.core.MethodIntrospector;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.handler.AbstractHandlerMethodMapping;
+import org.springframework.web.util.ServletRequestPathUtils;
+
+public abstract class AbstractSpringComponentHandlerMapping<T> extends ApplicationObjectSupport implements HandlerMapping, InitializingBean {
+
+	private boolean detectHandlerMethodsInAncestorContexts = false;
+	
+	private MappingRegistry mappingRegistry = new MappingRegistry();
+	
+	/**
+	 * 
+	 * Detects handler methods at initialization.
+	 * 
+	 * @see #initHandlerMethods
+	 */
+	@Override
+	public void afterPropertiesSet() {
+		initHandlerMethods();
+	}
+
+	/**
+	 * Scan beans in the ApplicationContext, detect and register handler methods.
+	 * 
+	 * @see #getCandidateBeanNames()
+	 * @see #processCandidateBean
+	 * @see #handlerMethodsInitialized
+	 */
+	protected void initHandlerMethods() {
+		for (String beanName : getCandidateBeanNames()) {
+			processCandidateBean(beanName);
+		}
+		handlerMethodsInitialized(getHandlerMethods());
+	}
+	
+	/**
+	 * Invoked after all handler methods have been detected.
+	 * @param handlerMethods a read-only map with handler methods and mappings.
+	 */
+	protected void handlerMethodsInitialized(Map<T, HandlerMethod> handlerMethods) {
+		// Total includes detected mappings + explicit registrations via registerMapping
+		int total = handlerMethods.size();
+		if ((logger.isTraceEnabled() && total == 0) || (logger.isDebugEnabled() && total > 0) ) {
+			logger.debug(total + " mappings in " + this.getClass().getCanonicalName());
+		}
+	}
+	
+	/**
+	 * Determine the names of candidate beans in the application context.
+	 * @since 5.1
+	 * @see #setDetectHandlerMethodsInAncestorContexts
+	 * @see BeanFactoryUtils#beanNamesForTypeIncludingAncestors
+	 */
+	protected String[] getCandidateBeanNames() {
+		return (this.detectHandlerMethodsInAncestorContexts ?
+				BeanFactoryUtils.beanNamesForTypeIncludingAncestors(obtainApplicationContext(), Object.class) :
+				obtainApplicationContext().getBeanNamesForType(Object.class));
+	}
+	
+	/**
+	 * Determine the type of the specified candidate bean and call
+	 * {@link #detectHandlerMethods} if identified as a handler type.
+	 * <p>This implementation avoids bean creation through checking
+	 * {@link org.springframework.beans.factory.BeanFactory#getType}
+	 * and calling {@link #detectHandlerMethods} with the bean name.
+	 * @param beanName the name of the candidate bean
+	 * @since 5.1
+	 * @see #isHandler
+	 * @see #detectHandlerMethods
+	 */
+	protected void processCandidateBean(String beanName) {
+		Class<?> beanType = null;
+		try {
+			beanType = obtainApplicationContext().getType(beanName);
+		}
+		catch (Throwable ex) {
+			// An unresolvable bean type, probably from a lazy bean - let's ignore it.
+			if (logger.isTraceEnabled()) {
+				logger.trace("Could not resolve type for bean '" + beanName + "'", ex);
+			}
+		}
+		if (beanType != null && isHandler(beanType)) {
+			detectHandlerMethods(beanName);
+		}
+	}
+	
+	/**
+	 * Whether the given type is a handler with handler methods.
+	 * @param beanType the type of the bean being checked
+	 * @return "true" if this a handler type, "false" otherwise.
+	 */
+	protected abstract boolean isHandler(Class<?> beanType);
+	
+	/**
+	 * Return a (read-only) map with all mappings and HandlerMethod's.
+	 */
+	public Map<T, HandlerMethod> getHandlerMethods() {
+		this.mappingRegistry.acquireReadLock();
+		try {
+			return Collections.unmodifiableMap(
+					this.mappingRegistry.getRegistrations().entrySet().stream()
+							.collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().handlerMethod)));
+		}
+		finally {
+			this.mappingRegistry.releaseReadLock();
+		}
+	}
+
+	/**
+	 * Look for handler methods in the specified handler bean.
+	 * @param handler either a bean name or an actual handler instance
+	 * @see #getMappingForMethod
+	 */
+	protected void detectHandlerMethods(Object handler) {
+		Class<?> handlerType = (handler instanceof String ?
+				obtainApplicationContext().getType((String) handler) : handler.getClass());
+
+		if (handlerType != null) {
+			Class<?> userType = ClassUtils.getUserClass(handlerType);
+			Map<Method, T> methods = MethodIntrospector.selectMethods(userType,
+					(MethodIntrospector.MetadataLookup<T>) method -> {
+						try {
+							return getMappingForMethod(method, userType);
+						}
+						catch (Throwable ex) {
+							throw new IllegalStateException("Invalid mapping on handler class [" +
+									userType.getName() + "]: " + method, ex);
+						}
+					});
+			if (logger.isTraceEnabled()) {
+				logger.trace(formatMappings(userType, methods));
+			}
+			
+			methods.forEach((method, mapping) -> {
+				Method invocableMethod = AopUtils.selectInvocableMethod(method, userType);
+				registerHandlerMethod(handler, invocableMethod, mapping);
+			});
+		}
+	}
+
+	protected void registerHandlerMethod(Object handler, Method method, T mapping) {
+		this.mappingRegistry.register(mapping, handler, method);
+	}
+
+	
+	
+	@Nullable
+	protected abstract T getMappingForMethod(Method method, Class<?> handlerType);
+	
+	private String formatMappings(Class<?> userType, Map<Method, T> methods) {
+		String formattedType = Arrays.stream(ClassUtils.getPackageName(userType).split("\\."))
+				.map(p -> p.substring(0, 1))
+				.collect(Collectors.joining(".", "", "." + userType.getSimpleName()));
+		Function<Method, String> methodFormatter = method -> Arrays.stream(method.getParameterTypes())
+				.map(Class::getSimpleName)
+				.collect(Collectors.joining(",", "(", ")"));
+		return methods.entrySet().stream()
+				.map(e -> {
+					Method method = e.getKey();
+					return e.getValue() + ": " + method.getName() + methodFormatter.apply(method);
+				})
+				.collect(Collectors.joining("\n\t", "\n\t" + formattedType + ":" + "\n\t", ""));
+	}
+	
+	
+	/**
+	 * A registry that maintains all mappings to handler methods, exposing methods
+	 * to perform lookups and providing concurrent access.
+	 *
+	 * <p>Package-private for testing purposes.
+	 */
+	class MappingRegistry {
+
+		private final Map<T, MappingRegistration<T>> registry = new HashMap<>();
+
+		private final MultiValueMap<String, T> pathLookup = new LinkedMultiValueMap<>();
+
+		private final Map<HandlerMethod, CorsConfiguration> corsLookup = new ConcurrentHashMap<>();
+
+		private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+		/**
+		 * Return all registrations.
+		 * @since 5.3
+		 */
+		public Map<T, MappingRegistration<T>> getRegistrations() {
+			return this.registry;
+		}
+
+		/**
+		 * Return matches for the given URL path. Not thread-safe.
+		 * @since 5.3
+		 * @see #acquireReadLock()
+		 */
+		@Nullable
+		public List<T> getMappingsByDirectPath(ServerWebExchange exchange) {
+			String path = exchange.getRequest().getPath().pathWithinApplication().value();
+			return this.pathLookup.get(path);
+		}
+
+		/**
+		 * Return CORS configuration. Thread-safe for concurrent use.
+		 */
+		@Nullable
+		public CorsConfiguration getCorsConfiguration(HandlerMethod handlerMethod) {
+			HandlerMethod original = handlerMethod.getResolvedFromHandlerMethod();
+			return this.corsLookup.get(original != null ? original : handlerMethod);
+		}
+
+		/**
+		 * Acquire the read lock when using getMappings and getMappingsByUrl.
+		 */
+		public void acquireReadLock() {
+			this.readWriteLock.readLock().lock();
+		}
+
+		/**
+		 * Release the read lock after using getMappings and getMappingsByUrl.
+		 */
+		public void releaseReadLock() {
+			this.readWriteLock.readLock().unlock();
+		}
+
+		public void register(T mapping, Object handler, Method method) {
+			this.readWriteLock.writeLock().lock();
+			try {
+				HandlerMethod handlerMethod = createHandlerMethod(handler, method);
+				validateMethodMapping(handlerMethod, mapping);
+
+				Set<String> directPaths = AbstractHandlerMethodMapping.this.getDirectPaths(mapping);
+				for (String path : directPaths) {
+					this.pathLookup.add(path, mapping);
+				}
+
+
+				this.registry.put(mapping,
+						new MappingRegistration<>(mapping, handlerMethod, directPaths, corsConfig != null));
+			}
+			finally {
+				this.readWriteLock.writeLock().unlock();
+			}
+		}
+
+		private void validateMethodMapping(HandlerMethod handlerMethod, T mapping) {
+			MappingRegistration<T> registration = this.registry.get(mapping);
+			HandlerMethod existingHandlerMethod = (registration != null ? registration.getHandlerMethod() : null);
+			if (existingHandlerMethod != null && !existingHandlerMethod.equals(handlerMethod)) {
+				throw new IllegalStateException(
+						"Ambiguous mapping. Cannot map '" + handlerMethod.getBean() + "' method \n" +
+						handlerMethod + "\nto " + mapping + ": There is already '" +
+						existingHandlerMethod.getBean() + "' bean method\n" + existingHandlerMethod + " mapped.");
+			}
+		}
+
+		public void unregister(T mapping) {
+			this.readWriteLock.writeLock().lock();
+			try {
+				MappingRegistration<T> registration = this.registry.remove(mapping);
+				if (registration == null) {
+					return;
+				}
+
+				for (String path : registration.getDirectPaths()) {
+					List<T> mappings = this.pathLookup.get(path);
+					if (mappings != null) {
+						mappings.remove(registration.getMapping());
+						if (mappings.isEmpty()) {
+							this.pathLookup.remove(path);
+						}
+					}
+				}
+
+				this.corsLookup.remove(registration.getHandlerMethod());
+			}
+			finally {
+				this.readWriteLock.writeLock().unlock();
+			}
+		}
+	}
+
+
+	static class MappingRegistration<T> {
+
+		private final T mapping;
+
+		private final HandlerMethod handlerMethod;
+
+		private final Set<String> directPaths;
+
+		private final boolean corsConfig;
+
+		public MappingRegistration(
+				T mapping, HandlerMethod handlerMethod, @Nullable Set<String> directPaths, boolean corsConfig) {
+
+			Assert.notNull(mapping, "Mapping must not be null");
+			Assert.notNull(handlerMethod, "HandlerMethod must not be null");
+			this.mapping = mapping;
+			this.handlerMethod = handlerMethod;
+			this.directPaths = (directPaths != null ? directPaths : Collections.emptySet());
+			this.corsConfig = corsConfig;
+		}
+
+		public T getMapping() {
+			return this.mapping;
+		}
+
+		public HandlerMethod getHandlerMethod() {
+			return this.handlerMethod;
+		}
+
+		public Set<String> getDirectPaths() {
+			return this.directPaths;
+		}
+
+		public boolean hasCorsConfig() {
+			return this.corsConfig;
+		}
+	}
+
+	
+	@Override
+	public List<HandlerExecutor> getHandlers(Action a) {
+//		Object handler = getHandlerInternal(request);
+//		if (handler == null) {
+//			handler = getDefaultHandler();
+//		}
+//		if (handler == null) {
+//			return null;
+//		}
+//		// Bean name or resolved handler?
+//		if (handler instanceof String) {
+//			String handlerName = (String) handler;
+//			handler = obtainApplicationContext().getBean(handlerName);
+//		}
+//
+//		// Ensure presence of cached lookupPath for interceptors and others
+//		if (!ServletRequestPathUtils.hasCachedPath(request)) {
+//			initLookupPath(request);
+//		}
+//
+//		HandlerExecutionChain executionChain = getHandlerExecutionChain(handler, request);
+//
+//		if (logger.isTraceEnabled()) {
+//			logger.trace("Mapped to " + handler);
+//		}
+//		else if (logger.isDebugEnabled() && !DispatcherType.ASYNC.equals(request.getDispatcherType())) {
+//			logger.debug("Mapped to " + executionChain.getHandler());
+//		}
+//
+//		if (hasCorsConfigurationSource(handler) || CorsUtils.isPreFlightRequest(request)) {
+//			CorsConfiguration config = getCorsConfiguration(handler, request);
+//			if (getCorsConfigurationSource() != null) {
+//				CorsConfiguration globalConfig = getCorsConfigurationSource().getCorsConfiguration(request);
+//				config = (globalConfig != null ? globalConfig.combine(config) : config);
+//			}
+//			if (config != null) {
+//				config.validateAllowCredentials();
+//			}
+//			executionChain = getCorsHandlerExecutionChain(request, executionChain, config);
+//		}
+//
+//		return executionChain;
+		
+		return null;
+	}
+
+}
