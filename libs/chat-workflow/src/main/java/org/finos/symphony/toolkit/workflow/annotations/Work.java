2d3cec406b286126cbc40be40aeba4012bbab806
Rob Moffat <rob@kite9.com>
2021-07-12 14:58:36 +0100

    Added Indexed annotation to work

--- libs/chat-workflow/src/main/java/org/finos/symphony/toolkit/workflow/annotations/Work.java
+++ libs/chat-workflow/src/main/java/org/finos/symphony/toolkit/workflow/annotations/Work.java
@@ -3,6 +3,8 @@ package org.finos.symphony.toolkit.workflow.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import org.springframework.stereotype.Indexed;
+
 /**
  * This indicates that this object is a value object that should be 
  * sent into chats / returned from chats.  
@@ -13,6 +15,7 @@ import java.lang.annotation.RetentionPolicy;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Indexed
 public @interface Work {
 
 	/**
