bb75772feec6dff226929e744e0c9b4b82e882bf
Rob Moffat <rob@kite9.com>
2021-07-19 15:28:58 +0100

    Fixed attachment test

--- libs/chat-workflow/src/main/java/org/finos/symphony/toolkit/workflow/response/AttachmentResponse.java
+++ libs/chat-workflow/src/main/java/org/finos/symphony/toolkit/workflow/response/AttachmentResponse.java
@@ -6,43 +6,43 @@ import org.finos.symphony.toolkit.workflow.content.Content;
 public class AttachmentResponse extends MessageResponse {
 	
 	private final byte[] attachment;
-	private final String suffix;
+	private final String extension;
 	private final String name;
 	
-	public AttachmentResponse(Addressable stream, Content m, String template, byte[] attachment, String name, String suffix) {
+	public AttachmentResponse(Addressable stream, Content m, String template, byte[] attachment, String name, String extension) {
 		super(stream, m, template);
 		this.attachment = attachment;
 		this.name = name;
-		this.suffix = suffix;
+		this.extension = extension;
 	}
 
-	public AttachmentResponse(Addressable stream, Content m, byte[] attachment, String name, String suffix) {
+	public AttachmentResponse(Addressable stream, Content m, byte[] attachment, String name, String extension) {
 		super(stream, m);
 		this.attachment = attachment;
 		this.name = name;
-		this.suffix = suffix;
+		this.extension = extension;
 	}
 	
-	public AttachmentResponse(Addressable stream, byte[] attachment, String name, String suffix) {
+	public AttachmentResponse(Addressable stream, byte[] attachment, String name, String extension) {
 		super(stream, Content.empty());
 		this.attachment = attachment;
 		this.name = name;
-		this.suffix = suffix;
+		this.extension = extension;
 	}
 
-	public AttachmentResponse(Addressable stream, Object data, Content m, String template, byte[] attachment, String name, String suffix) {
+	public AttachmentResponse(Addressable stream, Object data, Content m, String template, byte[] attachment, String name, String extension) {
 		super(stream, data, m, template);
 		this.attachment = attachment;
 		this.name = name;
-		this.suffix = suffix;
+		this.extension = extension;
 	}
 
 	public byte[] getAttachment() {
 		return attachment;
 	}
 
-	public String getSuffix() {
-		return suffix;
+	public String getExtension() {
+		return extension;
 	}
 
 	public String getName() {
