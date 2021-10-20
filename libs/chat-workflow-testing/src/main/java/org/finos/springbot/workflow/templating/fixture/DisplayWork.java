2a7059d3c06d5f4bd2d769c5bd836b24401283d4
Rob Moffat <rob@kite9.com>
2021-10-20 16:30:11 +0100

    Creating Templating Tests

---
+++ libs/chat-workflow-testing/src/main/java/org/finos/springbot/workflow/templating/fixture/DisplayWork.java
@@ -0,0 +1,33 @@
+package org.finos.springbot.workflow.templating.fixture;
+
+import org.finos.springbot.workflow.annotations.Display;
+import org.finos.springbot.workflow.annotations.Work;
+
+@Work
+public class DisplayWork {
+
+	@Display(name = "Some crazy name")
+	String s;
+	
+	@Display(visible = false)
+	String invisible;
+
+	public String getS() {
+		return s;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+	public String getInvisible() {
+		return invisible;
+	}
+
+	public void setInvisible(String invisible) {
+		this.invisible = invisible;
+	}
+	
+	
+	
+}
---
+++ libs/chat-workflow-testing/src/main/java/org/finos/springbot/workflow/templating/fixture/EnumWork.java
@@ -0,0 +1,21 @@
+package org.finos.springbot.workflow.templating.fixture;
+
+import org.finos.springbot.workflow.annotations.Work;
+
+@Work
+public class EnumWork {
+	
+	public enum TrafficLights { RED, AMBER, GREEN }
+
+	TrafficLights s;
+
+	public TrafficLights getS() {
+		return s;
+	}
+
+	public void setS(TrafficLights s) {
+		this.s = s;
+	}
+	
+	
+}
