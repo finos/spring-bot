2dd3199788d156a49a3c900969952236f4501962
sureshrupnar <sureshrupnar@sureshs-mbp>
2021-11-09 08:28:22 +0530

    disabled maven deploy for KoreAI

--- tools/koreai/src/main/java/org/finos/symphony/toolkit/koreai/request/KoreAIRequesterImpl.java
+++ tools/koreai/src/main/java/org/finos/symphony/toolkit/koreai/request/KoreAIRequesterImpl.java
@@ -19,95 +19,81 @@ import org.springframework.util.StringUtils;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-
 /**
  * https://developer.kore.ai/docs/bots/channel-enablement/adding-webhook-channel/
  *
  * @author rodriva
  */
 public class KoreAIRequesterImpl implements KoreAIRequester, InitializingBean {
-	
-    private static final Logger LOG = LoggerFactory.getLogger(KoreAIRequesterImpl.class);
-	
-    private final KoreAIResponseHandler koreaiResponseParser;
-    private final KoreAIResponseBuilder koreAIResponseBuilder;
-    protected Client client;
-    protected JsonNodeFactory jnf;
-    protected String jwt;
-    protected String url;
-    
-    public KoreAIRequesterImpl(KoreAIResponseHandler koreaiResponseParser, 
-    		KoreAIResponseBuilder koreAIResponseBuilder, 
-    		String url, 
-    		JsonNodeFactory jsonNodeFactory, 
-    		String jwt) {
-        this.koreaiResponseParser = koreaiResponseParser;
-        this.koreAIResponseBuilder = koreAIResponseBuilder;
-        this.url = url;
-        this.jnf = jsonNodeFactory;
-        this.jwt = jwt;
-        this.client = createClient();
-    }
-    
-    protected Client createClient() {
-	String trustStorePath = System.getProperty("javax.net.ssl.trustStore");
-    	if(StringUtils.hasText(trustStorePath) && trustStorePath.contains("$JAVA_HOME")) {
-    		String java_home = System.getenv().get("JAVA_HOME");
-    		System.setProperty("javax.net.ssl.trustStore", trustStorePath.replace("$JAVA_HOME", java_home));
-    	}
-    	return ClientBuilder.newBuilder()
-    		.connectTimeout(15, TimeUnit.SECONDS)
-    		.readTimeout(15, TimeUnit.SECONDS)
-    		.build();
-    }
 
-    public void send(Address a, String symphonyQuery) {
-    	LOG.info("Request to Kore AI:\n Address={}\n url={}\n Message={} \n jwt={}", a, url, symphonyQuery, jwt);
-    	try {
+	private static final Logger LOG = LoggerFactory.getLogger(KoreAIRequesterImpl.class);
+
+	private final KoreAIResponseHandler koreaiResponseParser;
+	private final KoreAIResponseBuilder koreAIResponseBuilder;
+	protected Client client;
+	protected JsonNodeFactory jnf;
+	protected String jwt;
+	protected String url;
+
+	public KoreAIRequesterImpl(KoreAIResponseHandler koreaiResponseParser, KoreAIResponseBuilder koreAIResponseBuilder,
+			String url, JsonNodeFactory jsonNodeFactory, String jwt) {
+		this.koreaiResponseParser = koreaiResponseParser;
+		this.koreAIResponseBuilder = koreAIResponseBuilder;
+		this.url = url;
+		this.jnf = jsonNodeFactory;
+		this.jwt = jwt;
+		this.client = createClient();
+	}
+
+	protected Client createClient() {
+		String trustStorePath = System.getProperty("javax.net.ssl.trustStore");
+		if (StringUtils.hasText(trustStorePath) && trustStorePath.contains("$JAVA_HOME")) {
+			String java_home = System.getenv().get("JAVA_HOME");
+			System.setProperty("javax.net.ssl.trustStore", trustStorePath.replace("$JAVA_HOME", java_home));
+		}
+		return ClientBuilder.newBuilder().connectTimeout(15, TimeUnit.SECONDS).readTimeout(15, TimeUnit.SECONDS)
+				.build();
+	}
+
+	public void send(Address a, String symphonyQuery) {
+		LOG.info("Request to Kore AI:\n Address={}\n url={}\n Message={} \n jwt={}", a, url, symphonyQuery, jwt);
+		try {
 			koreaiResponseParser.handle(a, requestToKoreAI(a, symphonyQuery));
 		} catch (Exception e) {
-	    	LOG.error("Request to Kore AI FAILED:\n Address={}\n url={}\n Message={}", a, url, symphonyQuery);
-	    	LOG.error("Caused by: ", e);
+			LOG.error("Request to Kore AI FAILED:\n Address={}\n url={}\n Message={}", a, url, symphonyQuery);
+			LOG.error("Caused by: ", e);
 		}
-    
-    }
+
+	}
 
 	protected KoreAIResponse requestToKoreAI(Address a, String symphonyQuery) throws Exception {
 		Entity<Object> json = Entity.json(buildPayload(a, symphonyQuery));
 		String out = client.target(url).request()
-			.accept(MediaType.APPLICATION_JSON)
-			.header("Authorization", "Bearer " + jwt)
-			.header("Content-Type", "application/json")
-			.post(json)
-			.readEntity(String.class);
-				
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + jwt)
+				.header("Content-Type", "application/json")
+				.post(json)
+				.readEntity(String.class);
+
 		KoreAIResponse kr = koreAIResponseBuilder.formatResponse(out);
 		return kr;
 	}
- 
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		this.client = createClient();
 	}
-    
 
 	public Object buildPayload(Address a, String text) {
-        LOG.info("buildPayload for text={} address={}", text, a); 
+		LOG.info("buildPayload for text={} address={}", text, a);
 
-        ObjectNode payload = jnf.objectNode().put("to", "");
-        payload.set("session", jnf.objectNode().put("new", false));
-        payload.set("message", jnf.objectNode().put("text", text));
-        payload.set("from", jnf.objectNode()
-        		.put("id", String.valueOf(a.getUserId()))
-        		.set("userInfo", jnf.objectNode()
-        				.put("firstName", a.getFirstName())
-        				.put("lastName", a.getLastName())
-        				.put("email", a.getEmail())));
-        
-        LOG.info("Constructed Payload for KoreAI={}", payload);
-        return payload;
-    }
+		ObjectNode payload = jnf.objectNode().put("to", "");
+		payload.set("session", jnf.objectNode().put("new", false));
+		payload.set("message", jnf.objectNode().put("text", text));
+		payload.set("from", jnf.objectNode().put("id", String.valueOf(a.getUserId())).set("userInfo", jnf.objectNode()
+				.put("firstName", a.getFirstName()).put("lastName", a.getLastName()).put("email", a.getEmail())));
+
+		LOG.info("Constructed Payload for KoreAI={}", payload);
+		return payload;
+	}
 }
-
-   
