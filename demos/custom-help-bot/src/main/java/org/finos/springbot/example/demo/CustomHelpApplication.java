358e15b43be22a18537ebeb0b172076d1a68f3db
Rob Moffat <rob@kite9.com>
2022-06-22 10:30:32 +0100

    Added extra options for custom-help-bot

--- demos/custom-help-bot/src/main/java/org/finos/springbot/example/demo/CustomHelpApplication.java
+++ demos/custom-help-bot/src/main/java/org/finos/springbot/example/demo/CustomHelpApplication.java
@@ -1,21 +1,8 @@
 package org.finos.springbot.example.demo;
 
-import org.finos.springbot.workflow.actions.SimpleMessageAction;
-import org.finos.springbot.workflow.actions.consumers.ActionConsumer;
-import org.finos.springbot.workflow.content.Addressable;
-import org.finos.springbot.workflow.content.Message;
-import org.finos.springbot.workflow.conversations.AllConversations;
-import org.finos.springbot.workflow.response.MessageResponse;
-import org.finos.springbot.workflow.response.handlers.ResponseHandlers;
-import org.finos.springbot.workflow.welcome.RoomWelcomeEventConsumer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @Configuration
