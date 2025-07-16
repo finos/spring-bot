dcafc6199367d9e549b4de611d210b2d727a0b2f
Rob Moffat <rob@kite9.com>
2025-07-16 13:34:48 +0100

    Nearly fixed the constructor

--- tools/llm-bot/src/main/java/org/finos/springbot/tool/llm/LLMService.java
+++ tools/llm-bot/src/main/java/org/finos/springbot/tool/llm/LLMService.java
@@ -1,7 +1,12 @@
 package org.finos.springbot.tool.llm;
 
-public interface LLMService {
+import org.springframework.beans.factory.InitializingBean;
 
-	
+public interface LLMService extends InitializingBean {
+
+	public String getApiKey();
+
+	public void setApiKey(String apiKey);
+
 	public String getResponse(String request);
 }
